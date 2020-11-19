248cd9f4abb142f4ed21f0493ee3d52a69a6d2a6
BuildTools <unconfigured@null.spigotmc.org>
2020-11-19 08:44:08 +0000

    Changes to menu and Utils

--- src/main/java/com/qa/ims/IMS.java
+++ src/main/java/com/qa/ims/IMS.java
@@ -3,40 +3,46 @@ package com.qa.ims;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.qa.ims.connection.db_Connection;
 import com.qa.ims.controller.CrudController;
-import com.qa.ims.controller.MenuCus;
-import com.qa.ims.controller.MenuItem;
-import com.qa.ims.controller.MenuOrder;
+import com.qa.ims.controller.CustomerController;
+import com.qa.ims.controller.ItemController;
+import com.qa.ims.controller.OrderController;
+import com.qa.ims.dao.CustomerDAO;
+import com.qa.ims.dao.ItemDAO;
+import com.qa.ims.dao.OrderDAO;
+import com.qa.ims.util.DBUtils;
+
 import java.util.Scanner;
 
 public class IMS {
 	
 	public static final Logger LOGGER = LogManager.getLogger();
-	db_Connection DBInstance;
+	DBUtils DBInstance;
 	Scanner scanner = new Scanner(System.in);
-	private final MenuCus customers;
-	private final MenuItem items;
-	private final MenuOrder orders;
+	private final CustomerController customers;
+	private final ItemController items;
+	private final OrderController orders;
 
 	public IMS() {
-		
-		this.customers = new MenuCus();
-		this.items = new MenuItem();
-		this.orders = new MenuOrder();
+		final CustomerDAO cust= new CustomerDAO();
+		final ItemDAO item= new ItemDAO();
+		final OrderDAO order= new OrderDAO();
+		this.customers = new CustomerController(cust);
+		this.items = new ItemController(item);
+		this.orders = new OrderController(order);
 		
 	}
 
-	public void menu() {
+	public void systemStart() {
 		LOGGER.info("What is your username");
-		String username = scanner.nextLine();
-		LOGGER.info("What is your password");
-		String password = scanner.nextLine();
+		String username = "root";//scanner.nextLine();
+		LOGGER.info("What is your password \n");
+		String password = "Passwordunknown123!";//scanner.nextLine();
 
-		DBInstance = db_Connection.connect(username, password);
+		DBInstance = DBUtils.connect(username, password);
 		
 		LOGGER.info(" Select an option below:");
-		LOGGER.info("  1) Customer \n  2) Order \n  3) Item \n  4) EXIT");
+		LOGGER.info("  1) Customer \n  2) Order \n  3) Item \n  4) Exit \n   ");
 		
 		String select = scanner.nextLine().toLowerCase();
 			CrudController<?> active = null;
@@ -51,25 +57,28 @@ public class IMS {
 				active = this.orders;
 				break;
 			case "exit":
+				LOGGER.info("You Have logged out!");
 				return;
 			default:
 				break;
 			}
-
-		LOGGER.info("What would you like to do with " + select.toLowerCase() + ":");
-		LOGGER.info("  1) Create  2) Read  3) Update 4) Delete  5) Exit");
-		String option = scanner.nextLine().toLowerCase();
-		doAction(active, option);
+		boolean check=true;
+		while(check) {
+			LOGGER.info("What would you like to do with " + select.toLowerCase() + ":");
+			LOGGER.info("  1) Create \n  2) Read \n  3) Update \n  4) Delete \n  5) Exit \n ");
 			
+			String option = scanner.nextLine().toLowerCase();
+			doAction(active, option, check);
+		}
 	}
 
-	public void doAction(CrudController<?> crudController, String option) {
+	public void doAction(CrudController<?> crudController, String option, boolean check) {
 		switch (option) {
 		case "create":
 			crudController.create();
 			break;
 		case "read":
-			crudController.read();
+			crudController.readAll();
 			break;
 		case "update":
 			crudController.update();
@@ -78,9 +87,9 @@ public class IMS {
 			crudController.delete();
 			break;
 		case "exit":
+			systemStart();
 			break;
-		default:
-			break;
+
 		}
 	}
 
--- src/main/java/com/qa/ims/Runner.java
+++ src/main/java/com/qa/ims/Runner.java
@@ -1,22 +1,17 @@
-
 package com.qa.ims;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-
 public class Runner {
+
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	public static void main(String[] args) {
-		
-		LOGGER.info("Welcome to this Item Management System!");
-		IMS menu = new IMS();
-		menu.menu();
-		
-
-	  
-
+		IMS ims = new IMS();
+		LOGGER.info("Welcome to this IMS");
+		ims.systemStart();
+	
 	}
+
 }
